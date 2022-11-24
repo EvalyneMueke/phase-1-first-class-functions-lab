82dbf9fa806315d65cf6b3e1deab0747ec3fc9c5
Evalyne <evalyne.mueke@student.moringaschool.com>
2022-11-24 16:09:11 +0300

    Done Assignment

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution in this file!
+
+const returnFirstTwoDrivers = function (twoDrivers) {
+    let drivers = [...twoDrivers.slice(0, 2)];
+    return drivers;
+}
+
+const returnLastTwoDrivers = function(twoDrivers){
+    let drivers = [...twoDrivers.slice(twoDrivers.length -2)];
+    return drivers;
+}
+//we use the .length above because we dont know the no of names used therefore .length stands for all names
+
+const selectingDrivers =[returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(num){
+    return function(fare=5){
+        return fare *num;
+    }
+}
+
+const fareDoubler = function(fare){
+    return createFareMultiplier(2)(fare);
+} 
+//the (2) because the fare is double
+
+const fareTripler = function(fare){
+    return createFareMultiplier(3)(fare);
+}
+
+function selectDifferentDrivers(twoDrivers, fn){
+    return fn(twoDrivers);
+}
